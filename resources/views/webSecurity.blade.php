ca9e3cd5f56ac385890e9b4cb44dd7d27eacfa37
CuongRod9 <56809687+CuongRod9@users.noreply.github.com>
2020-05-22 17:42:04 +0700

    update form css

--- resources/views/webSecurity.blade.php
+++ resources/views/webSecurity.blade.php
@@ -1,30 +1,40 @@
 @extends('master')
 @section('content')
 <style>
-    div.divForm{
-        display: inline-block;
-        position: fixed;
-        top: 20%;
-        left: 0;
-        right: 0;
-        width: 30%;
-        margin: auto;
-        border: 1px solid grey;
-        background: linear-gradient(130deg,rgba(34,193,195,.367682107),#e6a919 81%);
+    .divForm{
+      display: inline-block;
+      position: fixed;
+      top: 20%;
+      left: 0;
+      right: 0;
+      width: 30%;
+      margin: auto;
+      border: 1px solid grey;
+      background: linear-gradient(130deg,rgba(34,193,195,.367682107),#e6a919 81%);
+    }
+    .divForm h1, form label, .btnCenter button{
+      font-weight: bold
+    }
+    .btnCenter{
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
 </style>
 <div class="divForm">
     <h1 style="text-align: center">Content Test</h1>
     <form action="" method="post" style="padding: 20px">
         <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" autofocus>
         </div>
         <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <textarea name="" id=""  class="form-control"></textarea>
+          <label for="content">Content</label>
+          <textarea name="content" id="content" class="form-control" placeholder="Enter Content" rows="10"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="btnCenter">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
     </form>
 </div>
 @endsection
